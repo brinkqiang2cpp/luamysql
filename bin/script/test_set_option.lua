602c07a7a36559df2588330e867ffcae6bf7a6ba
brinkqiang <brink.qiang@gmail.com>
2019-04-30 10:29:22 +0800

    add

---
+++ bin/script/test_set_option.lua
@@ -0,0 +1,24 @@
+local mysql = require 'luamysql'
+
+print('mysql client version:', mysql._VERSION)
+
+local flag = (mysql.CLIENT_MULTI_STATEMENTS | mysql.CLIENT_MULTI_RESULTS)
+local conf = 
+{
+    host = 'localhost',
+    user = 'root',
+    passwd = 'holyshit',
+    db = 'world',
+    client_flag = flag
+}
+
+local client = mysql.createClient()
+client:setCharset('utf8')
+client:setReconnect(true)
+client:setConnectTimeout(3)
+client:setReadTimeout(3)
+client:setWriteTimeout(3)
+client:setProtocol(mysql.PROTOCOL_TCP)
+client:setCompress()
+client:connect(conf)
+client:close()
