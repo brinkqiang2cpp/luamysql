c7e5bf675af5d8e8572505b93ca284e20b89fb3b
brinkqiang <brink.qiang@gmail.com>
2019-05-24 16:36:20 +0800

    add

--- bin/script/createdb.sql
+++ bin/script/createdb.sql
@@ -1,10 +1,6 @@
 CREATE DATABASE  IF NOT EXISTS `world` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `world`;
--- MySQL dump 10.13  Distrib 5.6.17, for Win64 (x86_64)
---
 -- Host: localhost    Database: world
--- ------------------------------------------------------
--- Server version	5.6.21-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -62,5 +58,3 @@ UNLOCK TABLES;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2014-11-24 23:46:35
